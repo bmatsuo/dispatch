7260c6a4f5d7a23606ab682fea143c9fc981a64d
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-06 21:34:44 -0700

    Add *some* actual documentation of the godu example.

--- examples/godu/doc.go
+++ examples/godu/doc.go
@@ -2,20 +2,25 @@
  *  Filename:    doc.go
  *  Author:      Bryan Matsuo <bmatsuo@soe.ucsc.edu>
  *  Created:     Tue Jul  5 22:13:49 PDT 2011
- *  Description: 
- *  Usage:       gotest
+ *  Description: Documentation of the dispatch example 'godu'
+ *  Usage:       godoc github.com/bmatsuo/dispatch/examples/godu
  */
 
 /*
-godirs does...
+Godu is a simple port of Unix du using a FIFO dispatch and filepath.Walk.
 
 Usage:
 
-    godirs [options] ARGUMENT ...
+    godirs [options] DIR
 
 Arguments:
 
+    DIR
+            A directory to recursive stat.
+
 Options:
+
+    None
 
 */
 package documentation
