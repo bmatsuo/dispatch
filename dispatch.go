2d87c3e9e89ae2ce5c0ebb13ca1566428ffb6742
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-07-06 20:54:18 -0700

    Fix godoc typo and reference examples.

--- dispatch.go
+++ dispatch.go
@@ -11,9 +11,11 @@
 
 //  Package dispatch provides goroutine dispatch and concurrency limiting.
 //  It provides an object Dispatch which is a queueing system for concurrent
-//  functions. It implements a dynamic limit on the number of routines it is
-//  runs simultaneously. It also implements an interface Queue, allowing for
-//  alternate queue implementations.
+//  functions. It implements a dynamic limit on the number of routines that
+//  it runs simultaneously. It also implements an interface Queue, allowing
+//  for alternate queue implementations.
+//
+//  See github.com/bmatsuo/dispatch/examples for usage examples.
 package dispatch
 import (
     "sync"
